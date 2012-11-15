4800c2b65a6801f6b5f0b41557ecb7ff20f1786c
Mike Meier <mike.meier@ibrows.ch>
2012-11-15 11:42:37 +0100

    add docs

--- Service/Transport.php
+++ Service/Transport.php
@@ -9,32 +9,54 @@ class Transport
      * @var Config 
      */
     protected $config;
-    
+
+    /**
+     * @param Config $config
+     */
     public function __construct(Config $config)
     {
         $this->config = $config;
     }
-    
+
+    /**
+     * @param Message $message
+     * @param User $user
+     */
     public function sendMessageToUser(Message $message, User $user)
     {
         $ch = $this->getCurlHandle();
     }
-    
+
+    /**
+     * @param array $messages
+     * @param User $user
+     */
     public function sendMessagesToUser(array $messages, User $user)
     {
         
     }
-    
+
+    /**
+     * @param Message $message
+     * @param array $users
+     */
     public function sendMessageToUsers(Message $message, array $users)
     {
         
     }
-    
+
+    /**
+     * @param array $messages
+     * @param array $users
+     */
     public function sendMessagesToUsers(array $messages, array $users)
     {
         
     }
-    
+
+    /**
+     * @return resource
+     */
     protected function getCurlHandle()
     {
         $ch = curl_init();
